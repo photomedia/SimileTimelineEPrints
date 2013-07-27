8f023c3e32b93183cf83b5624d4effac42c32851
Tomasz Neugebauer <tomasz@Tomaszs-MacBook-Pro.local>
2013-07-27 15:31:57 -0400

    Updated the call to get the fileLastUpdate on EprintsData.js file to a jquery ajax call

--- fileLastUpdate.php
+++ fileLastUpdate.php
@@ -3,5 +3,5 @@ $file = "eartextedata.js";
 if (file_exists($file)) {
     $lastUpdate= "Data refreshed every 30 days.  Showing data from: " . date ("F d Y H:i:s.", filemtime($file));
 }
-echo ('document.write(\''.$lastUpdate.'\');');
+echo ($lastUpdate);
 ?>
